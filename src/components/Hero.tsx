12aa74bb36f3d2ede6864b2358a1b71d896bf267
unknown <rajaulkarim2810@gmail.com>
2025-10-30 23:40:04 +0600

    social icon

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -165,7 +165,7 @@ const Hero = () => {
                   href="https://www.facebook.com/GulshanClubOympiad"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/10  transition-all"
+                  className="inline-flex items-center px-4 py-2 gap-2 justify-center  rounded-full bg-white/10  transition-all"
                   aria-label="Gulshan Club Facebook"
                 >
                   <svg
@@ -178,13 +178,14 @@ const Hero = () => {
                   >
                     <path d="M22.675 0H1.326C.593 0 0 .594 0 1.326V22.67c0 .73.593 1.324 1.326 1.324H12.82v-9.845H9.692v-3.834h3.128V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.797.143v3.24l-1.92.001c-1.504 0-1.796.715-1.796 1.763v2.313h3.59l-.467 3.834h-3.123V24h6.116C23.406 24 24 23.406 24 22.674V1.326C24 .594 23.406 0 22.675 0" />
                   </svg>
+                  <p className="text-white text-sm font-medium">Facebook</p>
                 </a>
                 {/* YouTube Icon */}
                 <a
-                  href="https://www.facebook.com/GulshanClubOympiad"
+                  href="https://www.youtube.com/@GulshanClubLtd"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="inline-flex items-center p-2 justify-center w-10 h-10 rounded-full bg-white/10  transition-all"
+                  className="inline-flex items-center px-4 py-2 gap-2 justify-center rounded-full bg-white/10  transition-all"
                   aria-label="Gulshan Club YouTube"
                 >
                   <svg
@@ -197,6 +198,7 @@ const Hero = () => {
                   >
                     <path d="M23.498 6.186a2.975 2.975 0 0 0-2.093-2.104C19.044 3.5 12 3.5 12 3.5s-7.044 0-9.405.582A2.975 2.975 0 0 0 .502 6.186C0 8.545 0 12 0 12s0 3.455.502 5.814a2.976 2.976 0 0 0 2.093 2.104C4.956 20.5 12 20.5 12 20.5s7.044 0 9.405-.582a2.975 2.975 0 0 0 2.093-2.104C24 15.455 24 12 24 12s0-3.455-.502-5.814ZM9.545 15.568V8.432l6.363 3.568-6.363 3.568Z" />
                   </svg>
+                  <p className="text-white text-sm font-medium">YouTube</p>
                 </a>
               </div>
             </div>
